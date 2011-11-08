0b3c943d7dae0b586179efc4fce060bd9aaca5ab
meteor02 <kmo634314@yahoo.co.jp>
2011-11-08 14:20:37 +0900

    2011/11/8 14:20 commits

--- TwitterClient_Ver2/src/com/android/TwitetrClient/example2/TwitterClient_Ver2Activity.java
+++ TwitterClient_Ver2/src/com/android/TwitetrClient/example2/TwitterClient_Ver2Activity.java
@@ -8,10 +8,6 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
 public class TwitterClient_Ver2Activity extends ListActivity {
@@ -22,44 +18,6 @@ public class TwitterClient_Ver2Activity extends ListActivity {
 	SharedPreferences pref;
 
     @Override
-    /*public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-
-		Button btn=(Button)findViewById(R.id.button2);
-		btn.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				// TODO 自動生成されたメソッド・スタブ
-				startActivity(new Intent(TwitterClient_Ver2Activity.this,twitterPreference.class));
-			}
-		});
-
-		pref=getSharedPreferences(AppConsts.PREFERENCES_NAME,MODE_PRIVATE);
-		if(pref == null){
-        	pref.edit().commit();
-        }
-
-		if(twitter_client_util.isConnect(pref.getString(AppConsts.PREF_STATUS, ""))){
-        	try{
-        	ResponseList<Status> TL;
-
-    		TL=twitter_client_util.getTL(pref);
-            createData(TL);
-
-            setContentView(R.layout.main);
-    		adapter=new TwitterAdapter(this,R.layout.list_at,list);
-
-    		ListView listview=(ListView)findViewById(R.id.listView1);
-    		listview.setAdapter(adapter);
-
-        	}catch(Exception e){
-        		Toast.makeText(this, "タイムラインを取得できませんでした", Toast.LENGTH_LONG).show();
-        	}
-        }else{
-        	startActivity(new Intent(TwitterClient_Ver2Activity.this,twitterPreference.class));
-        }
-	}*/
-
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
